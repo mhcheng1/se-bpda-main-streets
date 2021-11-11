da24400c5be0e9fb9f1ac278bd02701154304a1c
Robert Steger <rsteger@bu.edu>
2021-11-11 17:42:59 -0500

    Generated JSON file of the Washington mainstreet locations

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from flask_table import Table, Col
 from flask import Flask, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+import json
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -24,25 +25,35 @@ class ItemTable2(Table):
     name = Col('Name of Business')
     street = Col('Address')
 
+def create_geoJSON():
+    try:
+        g_json = dict()
+        g_json["type"] = "FeatureCollection"
+        g_json["features"] = []
+        items = db.session.query(Business.object_id, Business.name, Location.longitude, Location.lattitude).filter(Business.object_id == Location.b_id).all()
+        for row in items:
+            properties_set = {"object_id": row[0], "name": row[1]}
+            geometry_set = {"type": "Point", "coordinates": [float(row[2]), float(row[3])]}
+            temp_set = dict()
+            temp_set["type"] = "Feature"  
+            temp_set["properties"] = properties_set
+            temp_set["geometry"] = geometry_set
+            g_json["features"].append(temp_set)
+        geo_file = open("../geoJsonFiles/washington_gate_busi.json", "w")
+        json.dump(g_json, geo_file)
+        geo_file.close()
+        return "Worked!"
+    except Exception as e:
+        return(str(e))
+
 @app.route("/")
 def root_site():
         return "Main Page"
 
-@app.route("/open/business")
-def get_open_business():
+@app.route("/WashingtonGate")
+def generate_map():
     try:
-        items = Business.query.filter_by(status='Open').all()
-        table = ItemTable1(items)
-        return table.__html__()
+        return create_geoJSON()
     except Exception as e:
         return(str(e))
 
-@app.route("/business/location")
-def get_business_location():
-    try:
-        sql = text('SELECT B.name, L.street FROM business B, location L WHERE B.object_id=L.b_id')
-        items = db.engine.execute(sql)
-        table = ItemTable2(items)
-        return table.__html__()
-    except Exception as e:
-        return(str(e))
