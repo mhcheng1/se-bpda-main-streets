5c20108c818a668c9210ff49162eb3ad590c0125
ukamila <ukamila@bu.edu>
2021-12-06 01:07:41 -0500

    Updates

---
+++ precache-manifest.7553234902561b1c60d6d42fafdf1163.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec68e450ca0dd4fe728fde090e030387",
+    "url": "/se-bpda-main-streets/index.html"
+  },
+  {
+    "revision": "e799de41aa39f51bdc7e",
+    "url": "/se-bpda-main-streets/static/css/2.ea348644.chunk.css"
+  },
+  {
+    "revision": "d403c38c676f6da79c07",
+    "url": "/se-bpda-main-streets/static/css/main.64c5e426.chunk.css"
+  },
+  {
+    "revision": "e799de41aa39f51bdc7e",
+    "url": "/se-bpda-main-streets/static/js/2.fa86e53a.chunk.js"
+  },
+  {
+    "revision": "f0f3f7d327a078a43391b4006b2ed951",
+    "url": "/se-bpda-main-streets/static/js/2.fa86e53a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "866f1460cff580292014",
+    "url": "/se-bpda-main-streets/static/js/3.54e3f7cc.chunk.js"
+  },
+  {
+    "revision": "d403c38c676f6da79c07",
+    "url": "/se-bpda-main-streets/static/js/main.53236e43.chunk.js"
+  },
+  {
+    "revision": "133f807914fd95e1a8f6",
+    "url": "/se-bpda-main-streets/static/js/runtime-main.94c58229.js"
+  }
+]);
