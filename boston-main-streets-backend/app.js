645cdb7e106e5b1e71049ccde54e30304d78f414
Austin Wang <yyaustin@bu.edu>
2022-05-02 14:15:55 -0400

    heroku deployment

--- boston-main-streets-backend/app.js
+++ boston-main-streets-backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path')
 require('express-async-errors')
 // controllers
 const mapRouter = require('./controllers/mapRouter')
@@ -10,17 +11,13 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
+app.use('/api/map', mapRouter)
+app.use('/api/business', businessRouter)
+
 app.get('/*', async (req, res) => {
-    res.sendFile(path.join(__dirname, './build/index.html'), (error) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'), (error) => {
         res.status(500).send(error)
     })
 })
-
-app.get('/api', async (req, res) => {
-    res.send('<h1>Which api?</h1>')
-})
-
-app.use('/api/map', mapRouter)
-app.use('/api/business', businessRouter)
 
 module.exports = app
