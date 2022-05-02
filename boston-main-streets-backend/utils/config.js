40947b4ee736e6926e973cb5d929420abfd2a9d8
Austin Wang <yyaustin@bu.edu>
2022-05-02 14:01:25 -0400

    heroku deployment

--- boston-main-streets-backend/utils/config.js
+++ boston-main-streets-backend/utils/config.js
@@ -4,14 +4,14 @@ const { getDatabase, connectDatabaseEmulator } = require('firebase/database')
 
 const PORT = process.env.PORT || 3001
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
 }
 const fireApp = initializeApp(firebaseConfig)
 const fireDb = getDatabase(fireApp)
