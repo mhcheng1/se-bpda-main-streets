952aaca236d35aa7c71938edf655d6e328db68ee
zhunan0905 <andyz0905@outlook.com>
2022-05-03 15:29:01 -0400

    Trying to put spaces between boxex

--- boston-main-streets-frontend/src/features/BusinessBoard.js
+++ boston-main-streets-frontend/src/features/BusinessBoard.js
@@ -44,22 +44,22 @@ const BusinessesBoard = () => {
     return (
         <Row>
             <FormControl type="search" onChange={({target}) => setKeyWord(target.value)} placeholder="Search" value={keyWord} />
-            {businessData.map((business, index) => (
-                <CardItem key={index} title={<Button variant="link" onClick={() => handleView(business)}> {business.business_name.toUpperCase()} </Button>} text={`Address: ${business.street_address}`}>
-                    <Row>
-                        <Col>
-                            <Button variant="link" onClick={() => handleView()}>Website</Button>
-                        </Col>
-                        <Col>
-                            {
-                                user
-                                    ? <Button variant="link" onClick={() => handleUpdate(business)}>Update</Button>
-                                    : <Button variant="link" onClick={() => handleUpdate(business)} disabled>Update</Button>
-                            }
-                        </Col>
-                    </Row>
-                </CardItem>
-            ))}
+                {businessData.map((business, index) => (
+                    <CardItem key={index} title={<Button variant="link" onClick={() => handleView(business)}> {business.business_name.toUpperCase()} </Button>} text={`Address: ${business.street_address}`}>
+                        <Row style = {{"padding-bottom" : "40px"}}>
+                            <Col>
+                                <Button variant="link" onClick={() => handleView()}>Website</Button>
+                            </Col>
+                            <Col>
+                                {
+                                    user
+                                        ? <Button variant="link" onClick={() => handleUpdate(business)}>Update</Button>
+                                        : <Button variant="link" onClick={() => handleUpdate(business)} disabled>Update</Button>
+                                }
+                            </Col>
+                        </Row>
+                    </CardItem>
+                ))}
         </Row>
     )
 }
